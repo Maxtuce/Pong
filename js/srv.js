acea9f053a00053368a0e45d38bf759f02aeace2
Mathieu Madec <madec.mathieu@gmail.com>
2017-11-10 18:35:26 +0100

    make node server display Hello World instead of index.php

--- js/srv.js
+++ js/srv.js
@@ -1,11 +1,10 @@
 var http = require('http');
-var fs = require('fs');
 
-var server = http.createServer(function(req, res) {
-    fs.readFile('./index.php', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
+var server = http.createServer(function(req, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
 });
 var io = require('socket.io').listen(server);
 server.listen(8080);
